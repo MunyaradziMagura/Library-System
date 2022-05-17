80e93bccf6369465878309c07957b4c770fafb5a
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-05-17 17:01:05 +0930

    dvd class done

--- LibrarySystemSkeleton/book.h
+++ LibrarySystemSkeleton/book.h
@@ -4,12 +4,12 @@ class book :
     public item
 {
 private:
-    string authors;
-    string generes;
+    vector<string> authors;
+    vector<string> generes;
 public:
     book(int _registrationCode, string _title, int _year, string _type, bool _state) : item( _registrationCode, _title, _year, _type, _state) {}
-    string getAuthor() {return authors; }
-    string getGenre() { return generes; }
+    vector<string> getAuthor() {return authors; }
+    vector<string> getGenre() { return generes; }
     void setAuthor(string _authors) { authors = _authors; }
     void setGenre(string _generes) { generes = _generes; }
 };
--- LibrarySystemSkeleton/dvd.h
+++ LibrarySystemSkeleton/dvd.h
@@ -4,8 +4,21 @@ class dvd :
     public item
 {
 private:
+    vector<string> directors;
+    vector<string> actors;
+    vector<string> generes;
 public:
-    dvd(int _registrationCode, string _title, int _year, string _type, bool _state) : item(_registrationCode, _title, _year, _type, _state) {}
+    dvd(int _registrationCode, string _title, int _year, string _type, bool _state, vector<string> _directors, vector<string> _actors, vector<string> _generes) : item(_registrationCode, _title, _year, _type, _state) {
+        directors = _directors;
+        actors = _actors;
+        generes = _generes;
+    }
+    vector<string> getAuthor() { return actors; };
+    vector<string> getGenre() {return generes;};
+    vector<string> getDirectors() {return directors;};
 
+    void setAuthor(vector<string> _directors) { directors = _directors; };
+    void setGenre(vector<string> _actors) { actors = _actors; };
+    void setDirectors(vector<string> _generes) { generes = _generes; };
 };
 
