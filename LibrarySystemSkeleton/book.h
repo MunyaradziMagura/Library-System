b3b270ed9a90a8f9bcea55fd1e70b50c82a84a4e
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-05-17 17:02:27 +0930

    edited book constructor

--- LibrarySystemSkeleton/book.h
+++ LibrarySystemSkeleton/book.h
@@ -7,10 +7,13 @@ private:
     vector<string> authors;
     vector<string> generes;
 public:
-    book(int _registrationCode, string _title, int _year, string _type, bool _state) : item( _registrationCode, _title, _year, _type, _state) {}
+    book(int _registrationCode, string _title, int _year, string _type, bool _state, vector<string> _authors, vector<string> _generes) : item( _registrationCode, _title, _year, _type, _state) {
+        authors = _authors;
+        generes = _generes;
+    }
     vector<string> getAuthor() {return authors; }
     vector<string> getGenre() { return generes; }
-    void setAuthor(string _authors) { authors = _authors; }
-    void setGenre(string _generes) { generes = _generes; }
+    void setAuthor(vector<string> _authors) { authors = _authors; }
+    void setGenre(vector<string> _generes) { generes = _generes; }
 };
 
